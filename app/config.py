459530f8c5c8cbfc4337e853ac290bef5180011f
신동석 <96603945+dss1222@users.noreply.github.com>
2022-07-25 15:49:35 +0900

    mongoAtlas도입, 오타 수정

--- app/config.py
+++ app/config.py
@@ -1,10 +1,11 @@
+from app.secret_config import MongoAtlas
 
 
 class Config:
     SECRET = '1q2w3e4r1q2w3e4r'
     ALGORITHM = 'HS256'
     TESTING = False
-    MONGO_URI = 'mongodb://localhost:27017/practice'
+    MONGO_URI = 'mongodb+srv://' + MongoAtlas.secret_id + ':' + MongoAtlas.secret_password + '@cluster0.pw9pd.mongodb.net/?retryWrites=true&w=majority'
 
 
 class TestConfig(Config):
