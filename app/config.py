649a27ec557a13dbd83caa45cdd3727630086156
신동석 <96603945+dss1222@users.noreply.github.com>
2022-07-26 17:07:56 +0900

    다 수정함

--- app/config.py
+++ app/config.py
@@ -1,11 +1,10 @@
-from app.secret_config import MongoAtlas
 
 
 class Config:
     SECRET = '1q2w3e4r1q2w3e4r'
     ALGORITHM = 'HS256'
     TESTING = False
-    MONGO_URI = 'mongodb+srv://' + MongoAtlas.secret_id + ':' + MongoAtlas.secret_password + '@cluster0.pw9pd.mongodb.net/?retryWrites=true&w=majoritz'
+    MONGO_URI = 'mongodb+srv://test:test1234@cluster0.pw9pd.mongodb.net/?retryWrites=true&w=majority'
 
 
 class TestConfig(Config):
